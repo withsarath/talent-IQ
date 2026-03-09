072db792a120c6a7603c861666d7ce89705ed2c4
Sarath-33 <withsarath.m@gmail.com>
2026-03-09 21:03:57 +0530

    Refactor production static file serving in server.js for improved path handling

--- backend/src/server.js
+++ backend/src/server.js
@@ -40,10 +40,12 @@ app.get("/test", (req, res) => {
 //   });
 // }
 if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  const frontendPath = path.join(process.cwd(), "frontend", "dist");
+
+  app.use(express.static(frontendPath));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+    res.sendFile(path.join(frontendPath, "index.html"));
   });
 }
 
