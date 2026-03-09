bd841c337ebdcc9c7f82a9b0a3f5c4343ddf39c0
Sarath-33 <withsarath.m@gmail.com>
2026-03-09 20:49:23 +0530

    Refactor server.js for improved code consistency and formatting

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,12 +2,12 @@ import express from "express";
 import { ENV } from "./lib/env.js";
 import path from "path";
 import { connectDB } from "./lib/db.js";
-import cors from "cors"
-import { serve } from "inngest/express"
+import cors from "cors";
+import { serve } from "inngest/express";
 import { inngest, functions } from "./lib/inngest.js";
-import { clerkMiddleware } from '@clerk/express'
-import chatRoutes from "./routes/chatRoutes.js"
-import sessionRoutes from "./routes/sessionRoute.js"
+import { clerkMiddleware } from "@clerk/express";
+import chatRoutes from "./routes/chatRoutes.js";
+import sessionRoutes from "./routes/sessionRoute.js";
 
 const app = express();
 const PORT = ENV.PORT;
@@ -16,40 +16,45 @@ const __dirname = path.resolve();
 
 //middleware
 app.use(express.json());
-app.use(cors({
+app.use(
+  cors({
     origin: ENV.CLIENT_URL,
-    credentials: true
-}));
-app.use(clerkMiddleware())//when we use this we can access the req,auth()
+    credentials: true,
+  }),
+);
+app.use(clerkMiddleware()); //when we use this we can access the req,auth()
 
-app.use("/api/inngest", serve({ client: inngest, functions }))
-app.use("/api/chat", chatRoutes)
-app.use("/api/sessions", sessionRoutes)
-
+app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat", chatRoutes);
+app.use("/api/sessions", sessionRoutes);
 
 app.get("/test", (req, res) => {
-    res.status(201).json({ message: "testing 1" });
+  res.status(201).json({ message: "testing 1" });
 });
 
+// if (ENV.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
+//   app.get("/{*any}", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
 if (ENV.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
-    app.get("/{*any}", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    })
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+  });
 }
-
 
 const startServer = async () => {
-    try {
-        await connectDB();
-        app.listen(PORT, () => {
-            console.log(`Port is listening to ${PORT}`);
-        });
-    } catch (error) {
-        console.log("Error at starting server", error);
-    }
-
-}
-startServer()
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Port is listening to ${PORT}`);
+    });
+  } catch (error) {
+    console.log("Error at starting server", error);
+  }
+};
+startServer();
